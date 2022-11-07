a9905777a9cbe1a554779a61ca51e06462fa52c1
Igor-S-M <igorsousa3148@gmail.com>
2022-11-07 03:15:42 -0300

    validacao de dados incluida + get tweets

--- index.js
+++ index.js
@@ -10,25 +10,33 @@ const contas = []
 
 app.get("/tweets", (req, res) => {
 
-    res.send(tweets)
+    res.send(tweets.slice(-10).reverse())
 })
 
 app.post("/tweets", (req, res) => {
 
-    const {tweet} = req.body
+    const tweet = req.body
 
-    tweets.push(tweet)
-
-    res.send(tweet)
+    if (!tweet.username || !tweet.tweet) {
+        res.status(400).send("Todos os campos são obrigatórios!") //BAD REQUEST
+        return
+    } else {
+        tweets.push(tweet)
+        res.status(201).send("OK")
+    }
 })
 
 app.post("/sign-up", (req, res) => {
-    
+
     const conta = req.body
 
-    contas.push(conta)
-    
-    res.send(conta)
+    if (!conta.username || !conta.avatar) {
+        res.status(400).send("Todos os campos são obrigatórios!") //BAD REQUEST
+        return
+    } else {
+        contas.push(conta)
+        res.status(201).send("OK")
+    }
 })
 
 app.listen(5000)
